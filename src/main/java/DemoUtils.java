9a88bf6d0a36b3cb71efd2dda25259a306c3bdc7
Maru Sosa <soassi@outlook.com>
2022-10-18 10:29:20 -0500

    assertNull and assertNotNull

--- src/main/java/DemoUtils.java
+++ src/main/java/DemoUtils.java
@@ -1,7 +1,12 @@
-package main.java;
-
 public class DemoUtils {
     public int add(int a, int b) {
         return a + b;
+    }
+
+    public Object checkNull(Object obj){
+        if (obj != null) {
+            return obj;
+        }
+        return null;
     }
 }
--- src/test/java/DemoUtilsTest.java
+++ src/test/java/DemoUtilsTest.java
@@ -1,8 +1,6 @@
-package test.java;
-
-import main.java.DemoUtils;
-import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.Assertions.*;
 import org.testng.annotations.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class DemoUtilsTest {
 
@@ -11,7 +9,7 @@ public class DemoUtilsTest {
         //set up => create instance of the class to test
         DemoUtils demoUtils = new DemoUtils();
 
-        int expected = 6;
+        /*int expected = 6;
         int unexpected = 8;
 
         // execute => call the method you want to test
@@ -19,6 +17,23 @@ public class DemoUtilsTest {
 
         // assert => check the actual result and verify that it is the expected result
         Assertions.assertEquals(expected, actual, "2+4 must be 6");
-        Assertions.assertNotEquals(unexpected, actual, "2+4 must not be 8");
+        Assertions.assertNotEquals(unexpected, actual, "2+4 must not be 8");*/
+
+        //minimizing our code:
+        assertEquals(6, demoUtils.add(2,4), "2+4 must be 6");
+        assertNotEquals(8, demoUtils.add(2,4), "2+4 must not be 8");
+
+
+    }
+
+    @Test
+    void testNullAndNotNull() {
+        DemoUtils demoUtils = new DemoUtils();
+
+        String str1 = null;
+        String str2 = "marusosa";
+
+        assertNull(demoUtils.checkNull(str1), "Object should be null");
+        assertNotNull(demoUtils.checkNull(str2), "Object should not be null");
     }
 }
