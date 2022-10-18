32f354596d0b348522e5fb72258d74f544f1d2af
Maru Sosa <soassi@outlook.com>
2022-10-18 13:56:58 -0500

    JUnit Assertions: assertSame, assertNotSame}

--- src/test/java/DemoUtilsTest.java
+++ src/test/java/DemoUtilsTest.java
@@ -2,7 +2,6 @@ import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class DemoUtilsTest {
     DemoUtils demoUtils;
 
@@ -12,39 +11,33 @@ public class DemoUtilsTest {
     }
 
     @Test
-    //@DisplayName("Equals and not equals")
-    void test_Equals_And_Not_Equals() {
-        //set up => create instance of the class to test
-        //DemoUtils demoUtils = new DemoUtils();
-
-        /*int expected = 6;
-        int unexpected = 8;
-
-        // execute => call the method you want to test
-        int actual = demoUtils.add(2, 4);
-
-        // assert => check the actual result and verify that it is the expected result
-        Assertions.assertEquals(expected, actual, "2+4 must be 6");
-        Assertions.assertNotEquals(unexpected, actual, "2+4 must not be 8");*/
-
-        //minimizing our code:
+    @DisplayName("Equals and not equals")
+    void testEqualsAndNotEquals() {
         assertEquals(6, demoUtils.add(2,4), "2+4 must be 6");
         assertNotEquals(8, demoUtils.add(2,4), "2+4 must not be 8");
-
-
     }
 
     @Test
-    //@DisplayName("Null and not null")
-    void test_Null_And_Not_Null() {
-        //DemoUtils demoUtils = new DemoUtils();
-
+    @DisplayName("Null and not null")
+    void testNullAndNotNull() {
         String str1 = null;
         String str2 = "marusosa";
 
         assertNull(demoUtils.checkNull(str1), "Object should be null");
         assertNotNull(demoUtils.checkNull(str2), "Object should not be null");
     }
+
+    @DisplayName("Same and not same")
+    @Test
+    void testSameAndNotSame() {
+        String str = "Test same and not same";
+        assertSame(demoUtils.getAcademy(), demoUtils.getAcademyDuplicate(),
+                "Object should refer to same object");
+        assertNotSame(str, demoUtils.getAcademy(), "" +
+                "Objects should not refer to same object");
+    }
+
+
 
     /*@AfterEach
     void tearDownAfterEach() {
