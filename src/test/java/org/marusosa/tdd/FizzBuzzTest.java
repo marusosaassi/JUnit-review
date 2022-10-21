32d2b350307c0665ce90081a8e083bdfb052a908
Maru Sosa <soassi@outlook.com>
2022-10-21 14:06:19 -0500

    Parametrized tests

--- src/test/java/org/marusosa/tdd/FizzBuzzTest.java
+++ src/test/java/org/marusosa/tdd/FizzBuzzTest.java
@@ -1,6 +1,8 @@
 package org.marusosa.tdd;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -39,6 +41,33 @@ public class FizzBuzzTest {
     void testForNotDivisibleByThreeOrFive() {
         String expected = "1";
         assertEquals(expected, FizzBuzz.compute(1), "should return 1");
+    }
+
+    @DisplayName("Testing with small data file")
+    @ParameterizedTest(name="value={0}, expected={1}")
+    //because we are passing parameters for this test (through an csv file)
+    @CsvFileSource(resources = "/small-test-data.csv")
+    @Order(5)
+    void testWithSmallDataFile(int value, String expected) {
+        assertEquals(expected, FizzBuzz.compute(value));
+        //behind the scenes, JUnit will run the test multiple times and supply the data
+        // for the parameters. JUnit does the looping.
+    }
+
+    @DisplayName("Testing with medium data file")
+    @ParameterizedTest(name="value={0}, expected={1}")
+    @CsvFileSource(resources = "/medium-test-data.csv")
+    @Order(6)
+    void testWithMediumDataFile(int value, String expected) {
+        assertEquals(expected, FizzBuzz.compute(value));
+    }
+
+    @DisplayName("Testing with large data file")
+    @ParameterizedTest(name="value={0}, expected={1}")
+    @CsvFileSource(resources = "/large-test-data.csv")
+    @Order(7)
+    void testWithLargeDataFile(int value, String expected) {
+        assertEquals(expected, FizzBuzz.compute(value));
     }
 
 }
